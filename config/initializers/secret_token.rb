f7e5ffa1e031b5cf74c5dd12f3a81467f7eca079
miau715 <miau715@techbang.com.tw>
2012-03-30 14:44:57 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneta::Application.config.secret_token = '987f1672688b43f3a8b5b5349e7ca14cc80e222bca660ba887ef3a49ceec0e0ea983b9b5d13ac2125526f8f91fdfb5d0f25f3599458195255a4e56b5f76b9053'
